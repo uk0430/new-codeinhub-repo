04ada52ef17cbd241d3f377b370fdf72509d5917
uk0430 <uk0430@icloud.com>
2019-07-23 20:54:04 -0400

    new commit

--- Hellow.java
+++ Hellow.java
@@ -3,6 +3,8 @@ package testcode;
 public class Hellow {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
